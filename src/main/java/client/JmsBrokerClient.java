ea352a9e9f02613e8d5c9b13cbb19d57c8764827
Benni <benjamin.ueber@campus.tu-berlin.de>
2023-06-20 13:05:43 +0200

    joa

--- src/main/java/client/JmsBrokerClient.java
+++ src/main/java/client/JmsBrokerClient.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -35,9 +36,15 @@ public class JmsBrokerClient {
         consumer = session.createConsumer(consumerQ);
         producer = session.createProducer(producerQ);
         listener = new MessageListener() {
-            public void onMessage(Message msg) {
-                if(msg instanceof ObjectMessage) {
-
+            public void onMessage(Message message) {
+                if(message instanceof ObjectMessage) {
+                    ListMessage msg = null;
+                    try {
+                        msg = (ListMessage) ((ObjectMessage) message).getObject();
+                        stocklist = msg.getStocks();
+                    } catch (JMSException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
         };
